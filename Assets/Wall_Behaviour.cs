30c1464a3151259180cd73b0a396f1374edc6338
ThatDudeGuy <waffles1999@icloud.com>
2024-04-07 16:59:27 -0400

    Tileset Changes

---
+++ Assets/Wall_Behaviour.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Wall_Behaviour : MonoBehaviour
+{
+    public GameObject player;
+    void Update()
+    {
+        //write code that will push the player slightly away from the wall so that
+        //the player is not stuck when moving into the wall
+
+        //or set the players movespeed to 0 when trying to move into the wall
+    }
+}
